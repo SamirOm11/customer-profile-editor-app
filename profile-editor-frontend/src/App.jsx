7fb6c1f5d29650bedbe3b0f4c0590b084fca6c04
samir <bante.samir@oranngemantra.in>
2025-04-09 18:20:33 +0530

    Initial commit

---
+++ profile-editor-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <p>Samir</p>
+    </>
+  );
+}
+
+export default App;
---
+++ profile-editor-frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    watch: {},
+    outDir: "../public/profile-editor",
+
+    rollupOptions: {
+      input: "/src/main.jsx",
+      external: ["@mui/material"],
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+});
